f756036c0f4a6bd6c035977654135704f26749cf
WilliamBostrom <133894488+WilliamBostrom@users.noreply.github.com>
2024-03-25 22:04:06 +0100

    Fixa kommentarer till projektsida på g

--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -1,30 +1,28 @@
 import { useParams } from "react-router-dom";
 import { useDocument } from "../../hooks/useDocument";
 
-/* // components
-import ProjectComments from "./ProjectComments"
-import ProjectSummary from "./ProjectSummary"
- */
+// components
+// import ProjectComments from "./ProjectComments";
+import ProjectSummary from "./ProjectSummary";
+
 // styles
 import "./Project.css";
 
-function Project() {
+export default function Project() {
   const { id } = useParams();
   const { document, error } = useDocument("projects", id);
 
   if (error) {
     return <div className="error">{error}</div>;
   }
-
   if (!document) {
     return <div className="loading">Loading...</div>;
   }
 
   return (
     <div className="project-details">
-      <h1>{document.name}</h1>
+      <ProjectSummary project={document} />
+      {/* <ProjectComments project={document} /> */}
     </div>
   );
 }
-
-export default Project;
