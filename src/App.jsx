f8125e2ce7ec303f5c09471f4674150cc4daa020
WilliamBostrom <133894488+WilliamBostrom@users.noreply.github.com>
2024-03-25 16:46:36 +0100

    login status

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Signup from "./pages/signup/Signup";
 import Project from "./pages/project/Project";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
-// import OnlineUsers from './components/OnlineUsers';
+import OnlineUsers from "./components/OnlineUsers";
 
 function App() {
   const { authIsReady, user } = useAuthContext();
@@ -50,7 +50,7 @@ function App() {
               />
             </Routes>
           </div>
-          {/* {user && <OnlineUsers />} */}
+          {user && <OnlineUsers />}
         </BrowserRouter>
       )}
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,8 @@
 import { NavLink } from "react-router-dom";
-// import { useAuthContext } from "../hooks/useAuthContext";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 // components
-// import Avatar from "./Avatar";
+import Avatar from "./Avatar";
 
 // styles & images
 import "./Sidebar.css";
@@ -10,14 +10,14 @@ import DashboardIcon from "../assets/dashboard_icon.svg";
 import AddIcon from "../assets/add_icon.svg";
 
 export default function Sidebar() {
-  // const { user } = useAuthContext();
+  const { user } = useAuthContext();
 
   return (
     <div className="sidebar">
       <div className="sidebar-content">
         <div className="user">
-          {/* <Avatar src={user.photoURL} /> */}
-          <p>Hey du!</p>
+          <Avatar src={user.photoURL} />
+          <p>Hey {user.displayName}</p>
         </div>
         <nav className="links">
           <ul>
