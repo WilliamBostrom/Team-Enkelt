ff8c60505249c53af8e46ec622df12af29474bab
WilliamBostrom <133894488+WilliamBostrom@users.noreply.github.com>
2024-03-25 21:40:10 +0100

    Visar doc på sidan

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,8 +1,30 @@
+import { useCollection } from "../../hooks/useCollection";
+import { useState } from "react";
+
+// components
+import ProjectList from "../../components/ProjectList";
+import ProjectFilter from "./ProjectFilter";
+
 // styles
 import "./Dashboard.css";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  const { documents, error } = useCollection("projects");
+  const [filter, setFilter] = useState("all");
+
+  const changeFilter = (newFilter) => {
+    setFilter(newFilter);
+  };
+  console.log(filter);
+
+  return (
+    <div>
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error}</p>}
+      {documents && <ProjectFilter changeFilter={changeFilter} />}
+      {documents && <ProjectList projects={documents} />}
+    </div>
+  );
 }
 
 export default Dashboard;
